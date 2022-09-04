b78658b0fa5a6c2a26009ff8a3faa36f9ae49677
kondrandr <andreymkt@yandex.ru>
2022-09-04 08:45:58 +0300

    Com1 Инициализация проекта
    Создание и инициализация проекта. Добавление всех пакетов и зависимостей. Создание Dockerfile в проект employee_service

---
+++ employee_service/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Employee.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Employee.Service
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public IConfiguration _configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<EmployeeDatabaseContext>(options =>
+            {
+              options
+                  .UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+            });
+
+      // AutoMapper
+      var mapperConfig = new MapperConfiguration(
+          mc =>
+          {
+            mc.AddProfile(new EmployeeMappingProfile());
+          }
+      );
+      services.AddSingleton(mapperConfig.CreateMapper());
+
+      services.AddScoped<IEmployeeService, EmployeeService>();
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "employee_service", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "employee_service v1"));
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
+
